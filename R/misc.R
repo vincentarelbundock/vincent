02e62307a2a3071279268a812b5c3f6557490429
Vincent Arel-Bundock <varel@umich.edu>
2016-10-06 11:38:08 -0400

    first commit

---
+++ R/misc.R
@@ -0,0 +1,130 @@
+#' Download and clean WDI data
+#'
+#' @param dictionary (named character vector)
+#' @export
+#' @examples
+#' GetWDI(dictionary=c("NY.GDP.MKTP.CD.XD"='deflator', "NY.GDP.PCAP.KD"='gdppc'))
+GetWDI = function(dictionary=c("NY.GDP.PCAP.KD"='gdppc')){
+    dat = WDI(indicator=names(dictionary), start=1960, end=2015, extra=TRUE)
+    dictionary = dictionary[names(dictionary) %in% colnames(dat)]
+    dat = dat[, c('iso3c', 'country', 'income', 'region', 'year', names(dictionary))]
+    colnames(dat) = c('iso3c', 'country', 'income', 'region', 'year', dictionary)
+    dat = dat[dat$region != 'Aggregates',]
+    dat$region = gsub(' \\(.*', '', dat$region)
+    dat$iso3n = countrycode(dat$iso3c, 'iso3c', 'iso3n')
+    dat = vincent::sort_df(dat, c('iso3c', 'iso3n', 'country', 'region', 'year', 'income'))
+    dat = dat[!is.na(dat$country),]
+    return(dat)
+}
+
+#' Find best fuzzy matches for each element of a vector with itself
+#'
+#' @param x strings to be matched (character)
+#' @param y strings to match to (character)
+#' @param matches how many strings to return? (integer)
+#' @export
+ClosestString = function(x, y, n=1){
+    dist = adist(x, y)
+    f = function(i) rank(i, ties.method='first')
+    g = function(i) y[order(i)]
+    dist = t(apply(dist, 1, f))
+    dist = t(apply(dist, 1, g))
+    out = data.frame(x, dist)
+    lb = ifelse(identical(x, y), 2, 1)
+    ub = min(lb + n, ncol(out))
+    out = out[, lb:ub]
+    colnames(out) = c('source', paste('matches', 1:(ncol(out)-1), sep='_'))
+    return(out)
+}
+
+#' Sorting a data.frame based on an index
+#'
+#' This function sorts the columns and rows of a data.frame using an index
+#' @param data data.frame to sort
+#' @param index sorting columns (character vector)
+#' @keywords sort data frame
+#' @export
+#' @examples
+#' sort_df(dat, c('iso3c_host', 'iso3c_home'))
+sort_df = function(data, index=NULL) {
+    vars = colnames(data)
+    vars = vars[!vars %in% index]
+    vars = sort(vars)
+    index = index[!index %in% vars]
+    out = data[, c(index, vars)]
+    out = sort(out, by=index)
+    return(out)
+}
+
+
+#' Write to CSV file with automatic date
+#'
+#' Writes to a CSV file with date automatically appended to file name
+#' @param data data.frame to write
+#' @param file filename to write to
+#' @param include row.names (boolean)
+#' @export
+#' @examples
+#' write_csv(dat, file="data/dataset")
+write_csv = function(data, file, row.names=FALSE) {
+    today = gsub('-', '', Sys.Date())
+    fn = paste(file, '_', today, '.csv', sep='')
+    write.csv(data, file=fn, row.names=row.names)
+}
+
+#' Studentize a variable
+#' @param x numeric vector to studentize
+#' @export
+studentize = function(x) {
+    out = (x - mean(x, na.rm=TRUE)) / sd(x, na.rm=TRUE)
+    return(out)
+}
+
+#' Recode a variable to 0-1 interval
+#' @param x numeric vector to studentize
+#' @export
+zero_one = function(x) {
+    out = x - min(x, na.rm=TRUE)
+    out = out / max(out, na.rm=TRUE)
+    return(out)
+}
+
+#' Read last CSV
+#'
+#' Globs file names, sorts and reads the last file. Useful when CSV file names
+#' have dates appended to them.
+#' @param file beginning of file name -- no extension (character)
+#' @export
+#' @examples
+#' read_last_csv('dataset')
+read_last_csv = function(file, ...) {
+    fn = Sys.glob(paste(file, '*.csv', sep=''))
+    fn = fn[length(fn)]
+    cat('\nLoading file: ', fn, '\n')
+    out = read.csv(fn, stringsAsFactors=FALSE, ...)
+    return(out)
+}
+
+#' HC-robust confidence interval
+#'
+#' Computes confidence intervals using the sandwich package
+#' @param object fitted model compatible with the sandwich package
+#' @export
+confint_robust <- function (object, parm, level = 0.95, ...)
+{
+    cf <- coef(object)
+    pnames <- names(cf)
+    if (missing(parm))
+        parm <- pnames
+    else if (is.numeric(parm))
+        parm <- pnames[parm]
+    a <- (1 - level)/2
+    a <- c(a, 1 - a)
+    pct <- stats:::format.perc(a, 3)
+    fac <- qnorm(a)
+    ci <- array(NA, dim = c(length(parm), 2L), dimnames = list(parm,
+        pct))
+    ses <- sqrt(diag(sandwich::vcovHC(object)))[parm]
+    ci[] <- cf[parm] + ses %o% fac
+    ci
+}
---
+++ R/school.R
@@ -0,0 +1,79 @@
+#' Normalize grades by 2nd/3rd quartiles
+#'
+#' @param data (data.frame with 3 columns: 'id', 'note', 'correcteur')
+#' @param plot (boolean plot or return raw data)
+#' @export
+NormalizeGrades = function(data, plot=FALSE){
+    if(ncol(data) != 3){
+        stop('Wrong number of columns')
+    }
+    if(any(colnames(data) != c('id', 'note', 'correcteur'))){
+        stop('Bad column names')
+    }
+    data = data %>% filter(!is.na(note))
+    # find larger quartiles
+    tar = data %>% na.omit %>% group_by(correcteur) %>% 
+          summarise(q25 = quantile(note, probs=.25, na.rm=TRUE),
+                    q75 = quantile(note, probs=.75, na.rm=TRUE)) %>%
+          select(-correcteur) %>% apply(., 2, max)
+    # normalize inter-grader results
+    f = function(x){
+        src = quantile(x, probs=c(.25, .75), na.rm=TRUE)
+        out = tar[1] + ((tar[2] - tar[1]) / (src[2] - src[1])) * (x - src[1])
+        return(out)
+    }
+    out = data %>% group_by(correcteur) %>%
+          mutate(note_norm = f(note))
+    out$note_norm = out$note
+    for(i in unique(out$correcteur)){
+        out$note_norm[out$correcteur == i] = f(out$note[out$correcteur == i])
+    }
+    out = out[, c('id', 'correcteur', 'note', 'note_norm')]
+    out$note_norm[out$note_norm < 0] = 0
+    out$note_norm[out$note_norm > 100] = 100
+    if(!plot){
+        return(out)
+    }else{
+        x = ggplot(out, aes(note, color=correcteur, linetype=correcteur)) + 
+            geom_density() + theme_minimal()
+        y = ggplot(out, aes(note_norm, color=correcteur, linetype=correcteur)) + 
+            geom_density() + theme_minimal()
+        z = ggplot(out, aes(note, note_norm, color=correcteur, linetype=correcteur)) + 
+            geom_point() + theme_minimal()
+        grid.arrange(x, y, z)
+    }
+}
+
+#' Convert percentage to letter grade
+#'
+#' @param index (character vector)
+#' @param slack (integer)
+#' @export
+pct2let = function(grades, slack=1.5){
+    bareme = structure(list(lb = c(90, 85, 80, 77, 73, 70, 65, 60, 57, 54, 
+    50, 35, 0.01, 0), ub = c(100, 89.99, 84.99, 79.99, 76.99, 72.99, 
+    69.99, 64.99, 59.99, 56.99, 53.99, 49.99, 34.99, 0.01), let = c("A+", 
+    "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "E", 
+    "F", "F*")), .Names = c("lb", "ub", "let"), class = "data.frame", row.names = c(NA, 
+    -14L))
+    bareme$lb = bareme$lb - slack
+    out = grades
+    for(i in nrow(bareme):1){
+        out[grades >= bareme$lb[i]] = bareme$let[i]
+    }
+    return(out)
+}
+
+#' Convert percentage to letter grade
+#'
+#' @param data (data.frame)
+#' @param index (vector of columns names)
+#' @export
+get_dups = function(data, index){
+    idx = data[, index]
+    idx = apply(idx, 1, paste, collapse='-')
+    idx = idx %in% idx[duplicated(idx)]
+    out = data[idx,]
+    out = vincent::sort_df(out, index)
+    return(out)
+}
